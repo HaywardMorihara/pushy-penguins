36145f8febe6aa2d0433ebc4249d2fb2aba05d18
HaywardMorihara <nathanielmorihara@gmail.com>
2019-09-12 23:32:03 -0400

    BasicMap and Tiles

--- core/assets/GrassTiles.tsx
+++ core/assets/GrassTiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.4" name="GrassTiles" tilewidth="16" tileheight="16" spacing="1" tilecount="9" columns="3">
- <image source="../development/personal/pushy-penguins/core/assets/GrassTiles.png" width="50" height="50"/>
+ <image source="GrassTiles.png" width="50" height="50"/>
 </tileset>
