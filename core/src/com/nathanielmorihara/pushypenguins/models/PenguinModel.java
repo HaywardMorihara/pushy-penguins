6c650343cc0707e3b7872dfd01da31ae52f915a0
HaywardMorihara <nathanielmorihara@gmail.com>
2019-11-03 15:24:34 -0500

    Unit Testing placeholder

--- core/src/com/nathanielmorihara/pushypenguins/models/PenguinModel.java
+++ core/src/com/nathanielmorihara/pushypenguins/models/PenguinModel.java
@@ -17,7 +17,7 @@ import com.badlogic.gdx.physics.box2d.World;
  */
 public class PenguinModel {
 
-  // TODO Make these based on spirte
+  // TODO Make these based on sprite
   private static final float minWidth = 1f; //24
   private static final float maxWidth = 1f; //60
   private static final float minHeight = 2f; //32
@@ -37,25 +37,20 @@ public class PenguinModel {
 
   // TODO Use scale
   public PenguinModel(World world, float scale, float x, float y) {
-    // First we create a body definition
     BodyDef bodyDef = new BodyDef();
-    // We set our body to dynamic, for something like ground which doesn't move we would set it to StaticBody
-    bodyDef.type = BodyDef.BodyType.KinematicBody;
+    bodyDef.type = BodyDef.BodyType.KinematicBody; // TODO What does this mean? Is this what I want?
     bodyDef.position.set(x, y);
 
-    // Create our body in the world using our body definition
     body = world.createBody(bodyDef);
 
     Random random = new Random();
     float randScale = random.nextFloat();
-    width = minWidth + randScale * (maxWidth - minWidth);
-    height = minHeight + randScale * (maxHeight - minHeight);
+    width = minWidth + randScale * scale * (maxWidth - minWidth);
+    height = minHeight + randScale * scale * (maxHeight - minHeight);
 
-    // Create a circle shape and set its radius to 6
     CircleShape circle = new CircleShape();
-    circle.setRadius(width/2);
+    circle.setRadius(width / 2);
 
-    // Create a fixture definition to apply our shape to
     FixtureDef fixtureDef = new FixtureDef();
     fixtureDef.shape = circle;
     fixtureDef.density = density;
---
+++ core/src/com/nathanielmorihara/pushypenguins/test/Test.java
@@ -0,0 +1,3 @@
+package com.nathanielmorihara.pushypenguins.test;
+
+// TODO
